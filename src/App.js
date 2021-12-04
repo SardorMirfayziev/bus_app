8b86da469a984f1858a64b3e2d32be590a823160
Sardor Mirfayziev <sardormirfayziev@gmail.com>
2021-12-04 20:19:08 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import "./App.scss";
+import "./App.css";
 import Orders from "./pages/orders";
 import Products from "./pages/Products";
-import onScan from "onscan.js";
 function App() {
-  useEffect(() => {
-    onScan.attachTo(document);
-  }, []);
+
   return (
     <div className="App">
       <div className="all">
         <Switch>
-          <Route path="/products/:id" component={Products} />
-          <Route exact path="/orders" component={Orders} />
-          <Redirect from="/" to="/orders" />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt aspernatur quo rem nihil. Tempore, doloremque cupiditate aspernatur dicta voluptas dolor soluta ipsa? Cupiditate harum ipsum magnam doloribus minima in ex!
         </Switch>
       </div>
     </div>
