b8c757c2bf2e1bb3f05ce3baf45ccae41e247aad
Sardor Mirfayziev <sardormirfayziev@gmail.com>
2021-12-04 20:28:09 +0500

    cahnge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Orders from "./pages/orders";
-import Products from "./pages/Products";
 function App() {
 
   return (
     <div className="App">
       <div className="all">
-        <Switch>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt aspernatur quo rem nihil. Tempore, doloremque cupiditate aspernatur dicta voluptas dolor soluta ipsa? Cupiditate harum ipsum magnam doloribus minima in ex!
-        </Switch>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt aspernatur quo rem nihil. Tempore, doloremque cupiditate aspernatur dicta voluptas dolor soluta ipsa? Cupiditate harum ipsum magnam doloribus minima in ex!
       </div>
     </div>
   );
